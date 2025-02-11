b6bafd45084a7f6d04669131fa85aeffc9b3c967
Andrew Johnson <andrewjohnso17@gmail.com>
2025-02-11 14:47:27 -0800

    configured isSelected code

--- src/game/WordTile.tsx
+++ src/game/WordTile.tsx
@@ -1,15 +1,20 @@
-import { BaseSyntheticEvent } from "react";
+import cx from "classnames";
 import "./WordTile.css";
 
+export type Word = { word: string; isSelected: boolean; idxPosition: number };
+
 type WordTileProps = {
-  word: string;
-  onSelectWord(event: BaseSyntheticEvent): void;
+  word: Word;
+  onSelectWord(word: Word): void;
 };
 
 const WordTile = ({ word, onSelectWord }: WordTileProps) => {
   return (
-    <li className="word-container" onClick={onSelectWord}>
-      {word}
+    <li
+      className={cx("word-container", { "is-selected": word.isSelected })}
+      onClick={() => onSelectWord(word)}
+    >
+      {word.word}
     </li>
   );
 };
